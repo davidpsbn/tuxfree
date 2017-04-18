1fdaca8c3669ed57a553ab2ac186b0de771547d5
davidpsbn <dps@davidweb.design>
2017-04-18 18:26:59 +0100

    Add files via upload

--- Tux/assets/js/app.js
+++ Tux/assets/js/app.js
@@ -2,35 +2,40 @@ $( document ).ready(function(){
     $(".quiz .quiz__step .yes").on('click', function(){
         $(this).parents('.quiz__step').next('.quiz__step').addClass('show');
         $(this).siblings('a').css("display","none");
-        $(this).addClass("zoom");
-        $(this).siblings('p').addClass("zoom");
-        $(this).parents('.quiz__step').animate({
-            left: 430,
-            top: 200,
-            height: 200,
-            width: 200,
-        }, 1000, 'easeInOutSine');
+        $(this).parents('.quiz__step').addClass('selected');
     }); 
     
      $(".quiz .quiz__step:nth-child(2) .yes").on('click', function(){
-        $(this).parents('.quiz__step').prev('.quiz__step').animate({
-            left: 300,
-            top: 225,
-            height: 150,
-            width: 150,
-        }, 1000);
-        $(".quiz .quiz__step:nth-child(1) p, a").addClass("zoom--xl");
+        $(this).parents('.quiz__step').prev('.quiz__step').addClass('selected--sec');
     }); 
 
     $(".quiz .quiz__step .restart").on('click', function(){
-        $(this).parents('.quiz__step').prev('.quiz__step').animate({
-            left: '50%',
-            top: 0,
-            height: 600,
-            width: 600,
-        }, 1000, 'easeInOutSine');
+        $(this).parents('.quiz__step').prev('.quiz__step').removeClass('selected','selected--sec');
         $(this).parents('.quiz__step').removeClass('show');
         $('.quiz__step .no').css('display','inline-block');
-        $('.quiz__step p, a').removeClass('zoom');
+
     });
+
+    //When choosing NO
+    $(".quiz .quiz__step .no").on('click', function(){
+        $(this).parents('.quiz__step').addClass('selected');
+        $(this).parents('.quiz__step').siblings('.quiz__step--not').addClass('show');
+        $(this).siblings('a').css("display","none");
+    }); 
+     $(".quiz .quiz__step--not .zerob, .one, .minus-equal-two").on('click', function(){
+        $('.selected').addClass('selected--sec');
+        $(this).siblings('a').css("display","none");
+        $(this).parents('.quiz__step').addClass('selected');
+        $(this).parents('.quiz__step').next('.quiz__step').addClass('show');
+    }); 
+
+    $(".quiz .quiz__step--no .restart").on('click', function(){
+        $('.quiz__step .one, .minus-equal-two   ').css('display','inline-block');
+        $(this).parents('.quiz__step').removeClass('show');
+         $(this).parents('.quiz__step').prev('.quiz__step').removeClass('show');
+        $('.show').removeClass('selected selected--sec');
+        $(this).parents('.quiz__step').removeClass('show');
+        $('.quiz__step .yes').css('display','inline-block');
+    });
+
 }); 
