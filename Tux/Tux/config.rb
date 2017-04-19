cf9cdd451179be3b222a06c1e261a3845f1bcb21
davidpsbn <dps@davidweb.design>
2017-04-19 11:29:14 +0100

    Add files via upload

---
+++ Tux/Tux/config.rb
@@ -0,0 +1,16 @@
+# Compass CSS framework config file
+
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "assets/scss"
+css_dir = "assets/css"
+images_dir = "assets/img"
+fonts_dir = "assets/fonts"
+javascripts_dir = "assets/js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
